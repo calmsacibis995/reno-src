3f24124b8237d6abe89c1bd51347966463da5d60
calmsacibis995 <www.apostolosstefanidis@gmail.com>
2022-09-18 11:59:32 +0300

    Source code upload

---
+++ sys/GENERIC.alltahoe/vers.c
@@ -0,0 +1,2 @@
+char sccs[] = "@(#)4.3 BSD Reno #6: Sat Jul 28 12:27:45 PDT 1990 (bostic@okeeffe.Berkeley.EDU:/usr/src/sys/GENERIC.alltahoe)\n";
+char version[] = "4.3 BSD Reno UNIX #6: Sat Jul 28 12:27:45 PDT 1990\n    bostic@okeeffe.Berkeley.EDU:/usr/src/sys/GENERIC.alltahoe\n";
---
+++ sys/GENERIC.allvax/vers.c
@@ -0,0 +1,2 @@
+char sccs[] = "@(#)4.3 BSD Reno #4: Sat Jul 28 13:24:08 PDT 1990 (trent@kerberos.berkeley.edu:/nbsd/usr/src/sys/GENERIC.allvax)\n";
+char version[] = "4.3 BSD Reno UNIX #4: Sat Jul 28 13:24:08 PDT 1990\n    trent@kerberos.berkeley.edu:/nbsd/usr/src/sys/GENERIC.allvax\n";
